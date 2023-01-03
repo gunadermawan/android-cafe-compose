157834b9740f5b1750c91fb2f22643ceb18a8d37
gunadermawan <gunadermawan.official@gmail.com>
2023-01-03 22:18:19 +0700

    create menu item and vertical scroll

--- app/src/main/java/com/gunder/cafe/MainActivity.kt
+++ app/src/main/java/com/gunder/cafe/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -14,8 +16,12 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.gunder.cafe.model.Menu
+import com.gunder.cafe.model.dummyBestSellerMenu
 import com.gunder.cafe.model.dummyCategory
+import com.gunder.cafe.model.dummyMenu
 import com.gunder.cafe.ui.components.CategoryItem
+import com.gunder.cafe.ui.components.MenuItem
 import com.gunder.cafe.ui.components.SearchBar
 import com.gunder.cafe.ui.components.SectionText
 import com.gunder.cafe.ui.theme.CafeTheme
@@ -34,10 +40,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CafeApp() {
-    Column {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
         Banner()
         SectionText(stringResource(R.string.section_category))
         CategoryRow()
+        SectionText(stringResource(R.string.section_favorite_menu))
+        MenuRow(dummyMenu)
+        SectionText(stringResource(R.string.section_best_seller_menu))
+        MenuRow(dummyBestSellerMenu)
     }
 }
 
@@ -83,4 +93,18 @@ fun CategoryRowPreview() {
     CafeTheme {
         CategoryRow()
     }
+}
+
+@Composable
+fun MenuRow(listMenu: List<Menu>, modifier: Modifier = Modifier) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu)
+        }
+    }
+
 }
