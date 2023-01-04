c2bfffcfb456727fde352f651ce38c998b595c7f
gunadermawan <gunadermawan.official@gmail.com>
2023-01-04 08:33:27 +0700

    create bottom navigation

---
+++ app/src/main/java/com/gunder/cafe/model/BottomBarItem.kt
@@ -0,0 +1,6 @@
+package com.gunder.cafe.model
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomBarItem(val title: String, val icon: ImageVector)
