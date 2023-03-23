db8f2c288b126f09009e0df0b9269be1e737e15e
gunadermawan <gunadermawan.official@gmail.com>
2023-03-23 15:12:21 +0700

    review code: create menu row

--- app/src/main/java/com/gunder/cafe/MainActivity.kt
+++ app/src/main/java/com/gunder/cafe/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.gunder.cafe
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -14,8 +17,12 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.gunder.cafe.model.Menu
+import com.gunder.cafe.model.dummyBestSeller
 import com.gunder.cafe.model.dummyCategory
+import com.gunder.cafe.model.dummyMenu
 import com.gunder.cafe.ui.components.CategoryItem
+import com.gunder.cafe.ui.components.MenuItem
 import com.gunder.cafe.ui.components.SearchBar
 import com.gunder.cafe.ui.components.SectionText
 import com.gunder.cafe.ui.theme.CafeTheme
@@ -65,11 +72,28 @@ fun CategoryRowPreview() {
 }
 
 @Composable
+fun MenuRow(listMenu: List<Menu>, modifier: Modifier = Modifier) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu)
+        }
+    }
+}
+
+@Composable
 fun CafeApp() {
-    Column {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
         Banner()
         SectionText(stringResource(R.string.section_category))
         CategoryRaw()
+        SectionText(stringResource(R.string.section_favorite_menu))
+        MenuRow(dummyMenu)
+        SectionText(stringResource(R.string.section_best_seller_menu))
+        MenuRow(listMenu = dummyBestSeller)
     }
 }
 
