3b898e98fef762afab83007c2d5c32ab7be0f5f9
gunadermawan <gunadermawan.official@gmail.com>
2023-03-24 06:16:07 +0700

    review code: create bottomNav

--- app/src/main/java/com/gunder/cafe/MainActivity.kt
+++ app/src/main/java/com/gunder/cafe/MainActivity.kt
@@ -9,6 +9,11 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -16,11 +21,11 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.gunder.cafe.model.Menu
-import com.gunder.cafe.model.dummyBestSeller
-import com.gunder.cafe.model.dummyCategory
-import com.gunder.cafe.model.dummyMenu
-import com.gunder.cafe.ui.components.*
+import com.gunder.cafe.model.*
+import com.gunder.cafe.ui.components.CategoryItem
+import com.gunder.cafe.ui.components.HomeSection
+import com.gunder.cafe.ui.components.MenuItem
+import com.gunder.cafe.ui.components.SearchBar
 import com.gunder.cafe.ui.theme.CafeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,6 +36,69 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 CafeApp()
             }
+        }
+    }
+}
+
+
+@Composable
+fun CafeApp(modifier: Modifier = Modifier) {
+    Scaffold(bottomBar = { BottomBar() }) {
+        Column(
+            modifier = modifier
+                .verticalScroll(rememberScrollState())
+                .padding(it)
+        ) {
+            Banner()
+//        slot based layout
+            HomeSection(
+                title = stringResource(R.string.section_category),
+                content = { CategoryRaw() })
+            HomeSection(
+                title = stringResource(R.string.section_favorite_menu),
+                content = { MenuRow(dummyMenu) })
+            HomeSection(title = stringResource(R.string.section_best_seller_menu),
+                content = {
+                    MenuRow(dummyBestSeller)
+                })
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CafeAppDefaultPreview() {
+    CafeTheme {
+        CafeApp()
+    }
+}
+
+@Composable
+fun BottomBar(modifier: Modifier = Modifier) {
+    BottomNavigation(modifier = modifier) {
+        val navItem = listOf(
+            BottomBarItem(title = stringResource(R.string.menu_home), icon = Icons.Default.Home),
+            BottomBarItem(
+                title = stringResource(R.string.menu_favorite),
+                icon = Icons.Default.Favorite
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_profile),
+                icon = Icons.Default.AccountCircle
+            ),
+        )
+        navItem.map {
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        imageVector = it.icon,
+                        contentDescription = it.title
+                    )
+                },
+                selected = it.title == navItem[0].title,
+                onClick = {},
+                label = { Text(it.title) }
+            )
         }
     }
 }
@@ -62,12 +130,6 @@ fun CategoryRaw(modifier: Modifier = Modifier) {
 }
 
 @Composable
-@Preview(showBackground = true)
-fun CategoryRowPreview() {
-    CategoryRaw()
-}
-
-@Composable
 fun MenuRow(listMenu: List<Menu>, modifier: Modifier = Modifier) {
     LazyRow(
         horizontalArrangement = Arrangement.spacedBy(16.dp),
@@ -77,37 +139,5 @@ fun MenuRow(listMenu: List<Menu>, modifier: Modifier = Modifier) {
         items(listMenu, key = { it.title }) { menu ->
             MenuItem(menu)
         }
-    }
-}
-
-@Composable
-fun CafeApp() {
-    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-        Banner()
-//        slot based layout
-        HomeSection(
-            title = stringResource(R.string.section_category),
-            content = { CategoryRaw() })
-        HomeSection(
-            title = stringResource(R.string.section_favorite_menu),
-            content = { MenuRow(dummyMenu) })
-        HomeSection(title = stringResource(R.string.section_best_seller_menu),
-            content = {
-                MenuRow(dummyBestSeller)
-            })
-//        SectionText(stringResource(R.string.section_category))
-//        CategoryRaw()
-//        SectionText(stringResource(R.string.section_favorite_menu))
-//        MenuRow(dummyMenu)
-//        SectionText(stringResource(R.string.section_best_seller_menu))
-//        MenuRow(listMenu = dummyBestSeller)
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun CafeAppDefaultPreview() {
-    CafeTheme {
-        CafeApp()
     }
 }
